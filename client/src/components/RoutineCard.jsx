53f4e42fcf211b75441c2169206f15fa38c506bf
gjuancruz <tatugalaz@gmail.com>
2022-11-21 20:45:58 -0300

    routines page snippet v2

--- client/src/components/RoutineCard.jsx
+++ client/src/components/RoutineCard.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Text, View, Image, TextInput, TouchableOpacity as Button, StyleSheet } from 'react-native'
 
 
-const RoutineCard = (props) =>{
-    
-    return(
+const RoutineCard = (props) => {
+
+    return (
         <View>
-        <Text>{props.title}</Text>
-        <Text>{props.exercises}</Text>
-        <Text>{props.reps}</Text>
-        <Text>a</Text>
+            <Text>{props.title}</Text>
+            <Text>{props.exercises} {props.reps}</Text>
         </View>
     )
 }
--- client/src/components/RoutinesPage.jsx
+++ client/src/components/RoutinesPage.jsx
@@ -20,7 +20,7 @@ const routines = useSelector((state) => state.users.routines)
             {/* <RoutineCard title={routines[0].createdAt}></RoutineCard> */}
             {routines && routines.map((r)=>{
                 return(
-                    <RoutineCard title={r.title} exercises={r.exercises.map(e=> e.exercise).join(', ')} reps={r.exercises.map(e=> e.reps).join(', ')}></RoutineCard>
+                    <RoutineCard title={r.title} exercises={r.exercises.map(e=> e.exercise + ' ' + e.reps + '\n')} ></RoutineCard>
                 )
             })}
 
